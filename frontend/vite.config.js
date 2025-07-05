2a65356bfd314ee3badd87879aee1a15ae4e7090
rishiiih <rishirajsingh270406@gmail.com>
2025-07-05 09:05:41 +0530

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-
   plugins: [react(), tailwindcss()],
-
-  plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://localhost:5000',
     },
   },
-
 })
